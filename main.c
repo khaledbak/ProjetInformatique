2eeb88c6ce7661b22433bd2929bf78f1846ff484
khaledbak <khaled.bakir@edu.ece.fr>
2025-12-19 14:43:56 +0100

    initialisation du plateau et affichage
    - Ajout de la création du tableau qui prend en compte la suppressions des combinaisons au démarage
    - Ajout des couleurs pour mieux distinguer les éléments du plateau à l'aide des codes ANSI

--- main.c
+++ main.c
@@ -5,10 +5,22 @@
 #define NBLIGNES 25
 #define NBCOLONNES 45
 
+// ici on définit les couleurs qu'on pourra utiliser pour notre plateau
+#define RESET   "\033[0m"
+
+#define ROUGE   "\033[31m"
+#define VERT    "\033[32m"
+#define JAUNE   "\033[33m"
+#define BLEU    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define BLANC   "\033[37m"
+
+
 
 int combinaison_interdite(char tab[NBLIGNES][NBCOLONNES], int i, int j)
 {
-char x = tab[i][j];
+    char x = tab[i][j];
     // vérification de la génération à l'horizontale, pas plus de 4 lettre similaire qui s'enchainent
     if (j >= 3 && tab[i][j-1] == x && tab[i][j-2] == x && tab[i][j-3] == x)
         return 1;
@@ -29,11 +41,54 @@ void initialisation_plateau(char tab[NBLIGNES][NBCOLONNES])
     {
         for(int j = 0; j< NBCOLONNES; j++)
         {
-            do{
-            index = rand()%5;
-            tab[i][j]= fl_types[index];
-            }while(combinaison_interdite(tab, i,j));
-            printf("%c ", tab[i][j]);
+            do
+            {
+                index = rand()%5;
+                tab[i][j]= fl_types[index];
+            }
+            while(combinaison_interdite(tab, i,j));
+
+        }
+    }
+}
+
+// ici on rend une chaine statique ex : ROUGE  "\033[31m" pour définir la couleur des caractères F
+const char* couleur_item(char c)
+{
+    switch (c)
+    {
+    case 'F':
+        return ROUGE;
+    case 'P':
+        return BLEU;
+    case 'O':
+        return JAUNE;
+    case 'A':
+        return VERT;
+    case 'C':
+        return MAGENTA;
+    default:
+        return BLANC;
+    }
+}
+
+
+void affichage_plateau(char tab[NBLIGNES][NBCOLONNES])
+{
+
+
+    for(int i = 0; i< NBLIGNES; i++)
+    {
+        printf("|");
+        for(int j = 0; j<NBCOLONNES; j++)
+        {
+            printf("%s%c%s", couleur_item(tab[i][j]), tab[i][j], RESET);
+            printf("|");
+        }
+        printf(("\n"));
+        for(int l = 0; l<NBCOLONNES*2; l++)
+        {
+            printf("_");
         }
         printf("\n");
     }
@@ -44,4 +99,5 @@ int main()
     srand(time(NULL));
     char plateau[NBLIGNES][NBCOLONNES];
     initialisation_plateau(plateau);
+    affichage_plateau((plateau));
 }
